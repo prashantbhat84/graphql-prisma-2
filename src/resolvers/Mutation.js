eb33758a3a6045b4468bd724c9ab8085eff0c1f9
Prashant Bhat <prashantbhat84@gmail.com>
2020-04-10 14:56:26 +0530

    JWT token creration and login

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,133 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+const Mutation = {
+  async createUser(parent, args, { prisma }, info) {
+    if (args.data.password.length <= 6) {
+      throw new Error("Password field should be greater than 6");
+    }
+    const password = await bcrypt.hash(args.data.password, 10);
+    const user = await prisma.mutation.createUser({
+      data: {
+        ...args.data,
+        password,
+      },
+    });
+    return { user, token: jwt.sign({ userId: user.id }, "secret1") };
+  },
+  async loginUser(parent, args, { prisma }, info) {
+    const user = await prisma.query.user({ where: { email: args.data.email } });
+    if (!user) {
+      throw new Error("Unable to login");
+    }
+    const verifypassword = await bcrypt.compare(
+      args.data.password,
+      user.password
+    );
+    if (!verifypassword) {
+      throw new Error("Unable to login");
+    }
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, "secret1"),
+    };
+  },
+  async deleteUser(parent, args, { prisma }, info) {
+    return prisma.mutation.deleteUser({
+      where: {
+        id: args.id,
+      },
+    });
+  },
+  async updateUser(parent, args, { prisma }, info) {
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: args.id,
+        },
+        data: args.data,
+      },
+      info
+    );
+  },
+  createPost(parent, args, { prisma }, info) {
+    return prisma.mutation.createPost(
+      {
+        data: {
+          title: args.data.title,
+          body: args.data.body,
+          published: args.data.published,
+          author: {
+            connect: {
+              id: args.data.author,
+            },
+          },
+        },
+      },
+      info
+    );
+  },
+  deletePost(parent, args, { prisma }, info) {
+    return prisma.mutation.deletePost(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  updatePost(parent, args, { prisma }, info) {
+    return prisma.mutation.updatePost(
+      {
+        where: {
+          id: args.id,
+        },
+        data: args.data,
+      },
+      info
+    );
+  },
+  createComment(parent, args, { prisma }, info) {
+    return prisma.mutation.createComment(
+      {
+        data: {
+          text: args.data.text,
+          author: {
+            connect: {
+              id: args.data.author,
+            },
+          },
+          post: {
+            connect: {
+              id: args.data.post,
+            },
+          },
+        },
+      },
+      info
+    );
+  },
+  deleteComment(parent, args, { prisma }, info) {
+    return prisma.mutation.deleteComment(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  updateComment(parent, args, { prisma }, info) {
+    return prisma.mutation.updateComment(
+      {
+        where: {
+          id: args.id,
+        },
+        data: args.data,
+      },
+      info
+    );
+  },
+};
+
+export { Mutation as default };
